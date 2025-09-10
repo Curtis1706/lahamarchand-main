668e133ab0482478cc6b6b1a3f684740eaf59cc9
Curtis1706 <alhtdharry7@gmail.com>
2025-09-10 16:43:09 +0100

    Fix: Downgrade to Tailwind CSS v3 to resolve lightningcss build issues on Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -9,26 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  experimental: {
-    turbo: {
-      rules: {
-        '*.css': {
-          loaders: ['postcss-loader'],
-          as: '*.css',
-        },
-      },
-    },
-  },
-  webpack: (config, { isServer }) => {
-    // Fix for lightningcss on Vercel
-    if (isServer) {
-      config.externals = config.externals || []
-      config.externals.push({
-        'lightningcss': 'commonjs lightningcss',
-      })
-    }
-    return config
-  },
 }
 
 export default nextConfig
